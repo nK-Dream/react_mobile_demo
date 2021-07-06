157ceab6ab2f4823cd48158a0fb8d6ea6f55f7e9
J <oncwnuAtbq7RWXrDbsrMJw4vPdjA@git.weixin.qq.com>
2021-07-06 16:16:35 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
-import './App.css'
+import { Switch, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import User from './pages/User'
 
 export default class App extends Component {
   render() {
     return (
-      <div className="demo"></div>
+      <div>
+        <Switch>
+          <Route path="/login" component={Login}/>
+          <Route path="/user" component={User} />
+        </Switch>
+      </div>
     )
   }
 }
